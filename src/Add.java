09befcd6d12a98bd8c32ea5512b9b174c5dfd235
yavantika13 <yavantika165@gmail.com>
2025-04-09 11:29:55 +0530

    he

---
+++ src/Add.java
@@ -0,0 +1,11 @@
+public class Add {
+    public static void main(String[] args) {
+        int a=20;
+        int b=4;
+        System.out.println(+(a+b));
+        System.out.println(+(a-b));
+        System.out.println(+(a*b));
+        System.out.println(+(a/b));
+    }
+
+}
---
+++ src/Basic.java
@@ -0,0 +1,14 @@
+public class Basic
+{
+    public static void main(String[] args) {
+        boolean a=true;
+        if (a==true)
+        {
+            System.out.println("hello");
+        }
+        else{
+            System.out.println("hiii");
+        }
+    }
+}
+
