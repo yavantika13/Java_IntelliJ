ec952be24f43ee1dde59217ce2bebc894d36b198
yavantika13 <yavantika165@gmail.com>
2025-04-11 10:05:37 +0530

    hii

---
+++ src/Switch.java
@@ -0,0 +1,20 @@
+public class Switch
+{
+    public static void main(String[] args) {
+        int a=5;
+        switch (a) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                System.out.println("hello");
+                break;
+            case 5:
+                System.out.println("hiii");
+                break;
+            default:
+                System.out.println("byee");
+
+        }
+    }
+}
