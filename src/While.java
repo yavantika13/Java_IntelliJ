b2613be20ad7f31a296f4b84c5c966d428d36b55
yavantika13 <yavantika165@gmail.com>
2025-04-11 11:32:28 +0530

    hii

--- src/While.java
+++ src/While.java
@@ -1,10 +1,10 @@
 public class While
 {
     public static void main(String[] args) {
-        int a=10;
+        int a=0;
         while (a<10) {
-            System.out.println("a");
-
+            System.out.println("hii");
+            a++;
         }
     }
 }
