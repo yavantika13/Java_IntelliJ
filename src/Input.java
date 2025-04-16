de309f23aa1693d1e4949e63fc57076a9b927681
yavantika13 <yavantika165@gmail.com>
2025-04-16 13:34:16 +0530

    hi

---
+++ src/Input.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Input
+{
+    public static void main(String[] args) {
+     Scanner in= new Scanner(System.in);
+     System.out.println("Enter size of arr");
+     int n=in.nextInt();
+     int [] arr=new int[n];
+     System.out.println("Enter value of arr");
+     for(int i=0;i<n;i++){
+         arr[i]=in.nextInt();
+
+     }
+     for(int k=0;k<arr.length;k++){
+         System.out.println(arr[k]);
+     }
+    }
+
+}
