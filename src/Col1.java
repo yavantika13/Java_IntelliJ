a053b4903d9b07355b417122d8c1213e4b2c12a0
yavantika13 <yavantika165@gmail.com>
2025-05-27 10:45:42 +0530

    hhi

---
+++ src/Col1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Col1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(int i=0;i<10000;i++){
+            arr.add(i);
+
+        }
+        System.out.println(arr);
+    }
+}
--- src/Neww.java
+++ src/Neww.java
@@ -11,10 +11,14 @@ public class Neww
         arr.add("12");
         arr.add("13");
        arr.add(0,"23");
+      // arr.get(1);
+       arr.remove(1);
+      // arr.set(1);
+
       //  Collections.sort(arr);
         System.out.println(arr);
-        System.out.println(arr.get(1));
-        System.out.println(arr.remove(0));
+      //  System.out.println(arr.get(1));
+        //System.out.println(arr.remove(0));
        // System.out.println(arr.set());g
     }
 }
